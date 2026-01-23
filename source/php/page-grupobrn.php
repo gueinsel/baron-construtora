163d0b92cd1f5b0a062602fb1708633cee9df069
pdrfxptrem <dd4733339@gmail.com>
2026-01-22 23:46:17 -0300

    page-grupobrn

---
+++ source/php/page-grupobrn.php
@@ -0,0 +1,113 @@
+<?php the_post();
+
+get_header(); ?>
+
+<section class="secao-banner">
+    <div class="container-fluid px-0 position-relative">
+        <img src="img/banner_grupo_brn-scaled.webp" class="img-fluid w-100 banner-img" alt="Banner Aéreo Grupo BRN">
+    </div>
+    <div class="faixa-azul-separadora"></div>
+</section>
+
+<section class="secao-padrao bg-white secao-sede">
+    <div class="container mt-4">
+        
+        <div class="cabecalho-secao mb-5">
+            <h6 class="subtitulo">Excelência que constrói confiança</h6>
+            <h2 class="titulo">Grupo BRN: Solidez, Inovação e Compromisso</h2>
+        </div>
+
+        <div class="row align-items-center">
+            <div class="col-lg-7 px-0">
+                <img src="img/sede.webp" class="img-fluid w-100 shadow-sm img-sede" alt="Sede Grupo BRN">
+            </div>
+            <div class="col-lg-5">
+                <div class="box-azul-sobreposto shadow-lg">
+                    <p class="texto-descritivo">
+                        A Construtora BRN integra o Grupo BRN, que há mais de 25 anos contribui para o desenvolvimento socioeconômico do país. Com tradição, solidez e credibilidade, atua na construção de condomínios verticais, horizontais e bairros planejados, sempre unindo inovação e responsabilidade para transformar empreendimentos em verdadeiras moradias.
+                    </p>
+                    <a href="#" class="btn-saiba-mais">Saiba Mais</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="secao-padrao bg-cinza secao-historia">
+    <div class="container py-4">
+        
+        <h6 class="subtitulo mb-4">Uma história de sucesso</h6>
+
+        <div class="row align-items-center gx-5">
+            <div class="col-lg-5 mb-4 mb-lg-0">
+                <img src="img/BRN-Aracatuba-114-1.webp" class="img-fluid w-100 shadow-sm" alt="Casal BRN">
+            </div>
+            <div class="col-lg-7">
+                <h3 class="titulo-destaque mb-4">
+                    Mais de 15 anos construindo lares onde histórias únicas ganham vida, memórias são criadas e sonhos se transformam em realidade.
+                </h3>
+                <p class="texto-corrido">
+                    Especializado em condomínios e loteamentos, o Grupo BRN une planejamento estratégico, excelência em engenharia e inovação constante para criar empreendimentos que valorizam cidades e transformam a vida das pessoas. Sua atuação vai além da construção, incorporando soluções inteligentes, sustentabilidade e atenção aos detalhes em cada etapa do projeto à entrega.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="secao-padrao bg-white secao-numeros">
+    <div class="container py-4">
+        
+        <div class="text-center mb-5 mx-auto box-intro-numeros">
+            <h3 class="titulo-centralizado mb-3">Entre as maiores construtoras do Brasil</h3>
+            <p class="texto-intro">
+                Temos orgulho de ser uma força positiva em mais de 47 cidades do estado de São Paulo ao ajudar famílias a conquistarem o sonho da casa própria através do nosso grande compromisso com a comunidade e com práticas sustentáveis.
+            </p>
+        </div>
+
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 justify-content-center lista-icones">
+            <div class="col"><img src="img/I-ENTREGA-NO.png" class="img-fluid w-100 shadow-sm hover-zoom" alt="+8 Mil Lares"></div>
+            <div class="col"><img src="img/I-CITY-ON.png" class="img-fluid w-100 shadow-sm hover-zoom" alt="Presença em 47 cidades"></div>
+            <div class="col"><img src="img/I-CONTRUCAO-ON.png" class="img-fluid w-100 shadow-sm hover-zoom" alt="+1.55 milhão de m2"></div>
+            <div class="col"><img src="img/I-INTEC-ON.png" class="img-fluid w-100 shadow-sm hover-zoom" alt="Reconhecida INTEC Brasil"></div>
+            <div class="col"><img src="img/I-15ANOS-ON.png" class="img-fluid w-100 shadow-sm hover-zoom" alt="+15 anos de atuação"></div>
+        </div>
+
+    </div>
+</section>
+
+<section class="secao-padrao bg-white pb-5 secao-logos">
+    <div class="container pb-4">
+        <div class="row g-4">
+            
+            <div class="col-lg-6">
+                <div class="box-cinza h-100">
+                    <h5 class="titulo-box">Certificações</h5>
+                    
+                    <div class="row row-cols-3 g-3">
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-mid@2x.webp" class="img-fluid" alt="Mid"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-moore@2x.webp" class="img-fluid" alt="Moore"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-intec2025@2x.webp" class="img-fluid" alt="Intec"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-iso2015@2x.webp" class="img-fluid" alt="ISO"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-gptw@2x.webp" class="img-fluid" alt="GPTW"></div></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="box-cinza h-100">
+                    <h5 class="titulo-box">Parceiros</h5>
+                    
+                    <div class="row row-cols-3 g-3">
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-caixa@2x.webp" class="img-fluid" alt="Caixa"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-novocasapaulista@2x.webp" class="img-fluid" alt="Casa Paulista"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/imeg_bar-1.webp" class="img-fluid" alt="Nossa Casa"></div></div>
+                        <div class="col"><div class="card-logo"><img src="img/img-stamp-mcmv@2x.webp" class="img-fluid" alt="MCMV"></div></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php get_footer() ?>
